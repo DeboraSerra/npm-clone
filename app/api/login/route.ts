63ec30fb24ab091b4ac21d35bd9802d39173d87c
Debora Serra <debora.r.serra@gmail.com>
2025-11-19 12:45:09 -0800

    fix: add db

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,27 +1,23 @@
-import fs from "fs";
+import { neon } from "@neondatabase/serverless";
 
 export const POST = async (request: Request) => {
   const formData = await request.formData();
-  const username = formData.get("username");
-  const password = formData.get("password");
+  const username = String(formData.get("username") ?? "");
+  const password = String(formData.get("password") ?? "");
 
-  const usersFilePath = "data/users.json";
-  const usersData = fs.readFileSync(usersFilePath, "utf-8");
-  const users = JSON.parse(usersData);
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const user =
+    await sql`SELECT * FROM npm_clone WHERE username = ${username} AND password = ${password}`;
 
-  const user = users.find(
-    (u: { username: string; password: string }) =>
-      u.username === username && u.password === password
-  );
   console.log({ user: { username, password } });
-  if (user) {
+  if (user.length > 0) {
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   } else {
-    users.push({ username, password });
-    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+    await sql`INSERT INTO npm_clone (username, password) VALUES (${username}, ${password})`;
+
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
--- app/api/otp/route.ts
+++ app/api/otp/route.ts
@@ -1,28 +1,19 @@
-import fs from "fs";
+import { neon } from "@neondatabase/serverless";
 
 export const POST = async (request: Request) => {
   const formData = await request.formData();
   const username = formData.get("username");
   const otp = formData.get("otp");
 
-  const usersFilePath = "data/users.json";
-  const usersData = fs.readFileSync(usersFilePath, "utf-8");
-  const users = JSON.parse(usersData);
-
-  const user = users.find(
-    (u: { username: string; otp: string }) => u.username === username
-  );
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const [user] =
+    await sql`SELECT * FROM npm_clone WHERE username = ${username}`;
+  console.log({ user });
 
   console.log({ user: { username, otp, password: user.password } });
 
   if (user) {
-    const newUsers = users.map((u: { username: string }) => {
-      if (u.username === username) {
-        return { ...u, otp: otp };
-      }
-      return u;
-    });
-    fs.writeFileSync(usersFilePath, JSON.stringify(newUsers, null, 2));
+    await sql`UPDATE npm_clone SET otp = ${otp} WHERE username = ${username}`;
     return new Response(JSON.stringify({ success: true }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
